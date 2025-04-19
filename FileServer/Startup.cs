44516ed701f1057e81f0b1c3f8adc06331b6250e
FWink <12393262+FWink@users.noreply.github.com>
2025-04-19 19:41:22 +0200

    Fixed: zipped download does not work when hosting with Kestrel (sync IO)

--- FileServer/Startup.cs
+++ FileServer/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,7 +17,13 @@ namespace FileServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //synchronous IO is required for zipping.
+            //at least until https://github.com/dotnet/runtime/issues/1541 has been implemented (in .NET 10 it seems)
             services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+            services.Configure<KestrelServerOptions>(options =>
             {
                 options.AllowSynchronousIO = true;
             });
